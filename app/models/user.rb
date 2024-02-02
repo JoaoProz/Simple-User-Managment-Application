453dd775adf925ac8e91fafbcd91a32f3a3ee0bf
João Proença <joaopp1998@gmail.com>
2024-02-02 15:03:28 +0000

    Rubocop fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# User model
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
